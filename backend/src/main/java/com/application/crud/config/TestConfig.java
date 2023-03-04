a2cd3b300e42aebf6c5858524926e6013b2decb6
Gustavo Dorigon Coan <88582581+Gustavo-Dorigon-Coan@users.noreply.github.com>
2023-03-04 16:46:02 -0300

    Create endpoints for users

--- backend/src/main/java/com/application/crud/config/TestConfig.java
+++ backend/src/main/java/com/application/crud/config/TestConfig.java
@@ -1,8 +1,6 @@
 package com.application.crud.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
--- backend/src/main/java/com/application/crud/entities/User.java
+++ backend/src/main/java/com/application/crud/entities/User.java
@@ -22,6 +22,9 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 
+	public User() {
+		
+	}
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
